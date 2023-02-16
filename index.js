c0c3df8d5796475e536055fe009a9eb5f0a40820
VeronikaGazarek <veronika.gazarek@gmail.com>
2023-02-16 11:45:25 +0100

    added animated mode and light mode

---
+++ index.js
@@ -0,0 +1,12 @@
+const btnGroup = document.getElementById("btn-group")
+const styleTag = document.getElementById("style-mode")
+btnGroup.addEventListener("click", changeMode)
+
+function changeMode(event){
+    if(event.target.tagName === "BUTTON"){
+        let pathString = `./css/${event.target.id}.css`
+        console.log(pathString)
+        styleTag.setAttribute("href", pathString)
+    } //else if (event.target.tagName === "BUTTON" && pathString === `./css/styleAnim.css`) {
+       // document.getElementById('bubbles').style.display = 'block';}
+}
